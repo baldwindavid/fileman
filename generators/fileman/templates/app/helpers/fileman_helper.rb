5c93603386b23bb6d076cd332b0ca2450725902f
David Baldwin <baldwindavid@gmail.com>
2008-03-28 01:35:17 +0000

    added options for caption, title and icons

--- generators/fileman/templates/app/helpers/fileman_helper.rb
+++ generators/fileman/templates/app/helpers/fileman_helper.rb
@@ -6,6 +6,9 @@ module FilemanHelper
   #   _id and _type will automatically be added this name
   #   default is resource name + able => ex. attachment would become attachmentable (stupid)
   # :belongs_to => object that owns this resource
+  # :with_caption => true || false
+  # :with_icon => true || false
+  # :with_display_name => true || false
   
   # Example Model associations....
   # - has_many :documents, :as => :documentable, :dependent => :destroy
@@ -17,6 +20,9 @@ module FilemanHelper
     options[:polymorphic] ||= false
     options[:polymorphic_name] ||= "#{resource.downcase}able"
     options[:belongs_to] ||= false
+    options[:with_caption] ||= false
+    options[:with_icon] ||= true
+    options[:with_display_name] ||= true
     
     if action == :add
       fileman_add(resource, options)
@@ -36,15 +42,15 @@ module FilemanHelper
     haml_tag :ul, {:id => resource.tableize, :class => 'none'} do
       owned_resources = options[:belongs_to] ? eval("options[:belongs_to].#{resource.tableize}") : resource.constantize.find(:all)
       owned_resources.each do |item|
-        puts render :partial => "/fileman/list_item", :object => item, :locals => {:resource => resource }
+        puts(render :partial => "/fileman/list_item", :object => item, :locals => {:resource => resource, :options => options })
       end
     end  
   end
 
   def fileman_add(resource, options)
-    puts link_to_function "Add #{options[:resource_title]}", visual_effect(:toggle_appear, "#{resource.downcase}_new")
+    puts(link_to_function "Add #{options[:resource_title]}", visual_effect(:toggle_appear, "#{resource.downcase}_new"))
     haml_tag :div, {:id => "#{resource.downcase}_new", :style => 'display:none'} do
-      puts render :partial => "/fileman/new_form", :locals => {:options => options, :resource => resource}
+      puts(render :partial => "/fileman/new_form", :locals => {:options => options, :resource => resource})
     end
   end
 end
